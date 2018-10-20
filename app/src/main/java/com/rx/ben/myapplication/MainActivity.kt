22735cd5fdf51714276c63a93369bdfdfa0711ee
Benjamin Roux <benjamin2.roux@epitech.eu>
2018-10-20 23:30:52 +0100

    refactor code

--- app/src/main/java/com/rx/ben/myapplication/MainActivity.kt
+++ app/src/main/java/com/rx/ben/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val formattedDate = if (LocaleUtils.getLocale() == "en_US") DateUtils.getUSFormattedDate(Calendar.getInstance().time) else DateUtils.getEUFormattedDate(Calendar.getInstance().time)
+        val formattedDate = if (LocaleUtils.getLocale() == "en_US") DateUtils.getUSFormattedDate(DateUtils.getDate()) else DateUtils.getEUFormattedDate(DateUtils.getDate())
         binding.mainDate.text = String.format("Date : $formattedDate")
     }
 }
